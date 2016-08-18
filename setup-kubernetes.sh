017fa72046e03be9ea5ee4a15ef724a6024eacac
Ben Heasly <benjamin.heasly@gmail.com>
2016-08-18 13:18:29 -0400

    add pods for more docker containers

--- setup-kubernetes.sh
+++ setup-kubernetes.sh
@@ -1,5 +1,6 @@
 kubectl create -f volume-data-shared.yaml --validate=false
 kubectl	create -f claim-data-shared.yaml --validate=false
-kubectl	create -f mitsuba-spectral.yaml --validate=false
-
-kubectl describe all
+kubectl	create -f mitsuba-rgb.yaml --validate=false
+kubectl create -f mitsuba-spectral.yaml --validate=false
+kubectl create -f pbrt-v2-spectral.yaml --validate=false
+kubectl create -f exrtools.yaml --validate=false
--- teardown-kubernetes.sh
+++ teardown-kubernetes.sh
@@ -1,4 +1,3 @@
 kubectl delete --all replicationcontrollers
 kubectl delete --all pvc
 kubectl delete --all pv
-
